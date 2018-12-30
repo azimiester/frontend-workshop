8c75e9075b85d2844fce8b91b9979ca08ffecfff
Azeem Akhter <azeem.akhter@futurice.com>
2018-12-30 15:59:13 +0500

    Added router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import App from "./Components/App";
 
-render(<App />, document.getElementById("app"));
+const RoutedApp = () => (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/about" render={() => <h1>This is About page</h1>} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
+render(<RoutedApp />, document.getElementById("app"));
